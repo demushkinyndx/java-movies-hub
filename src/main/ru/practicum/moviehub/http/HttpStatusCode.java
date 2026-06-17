d6c82f917875d566f19a11c11a891aec5cf6e8ea
Andrey Demushkin <demushkin@gmail.com>
2026-06-17 17:32:58 +0300

    movies-hub v0.5 fix code style

--- src/main/ru/practicum/moviehub/http/HttpStatusCode.java
+++ src/main/ru/practicum/moviehub/http/HttpStatusCode.java
@@ -1,12 +1,12 @@
 package ru.practicum.moviehub.http;
 
 public class HttpStatusCode {
-final public static int UNPROCESSABLE_CONTENT = 422 ;
-final public static int UNSUPPORTED_MEDIA_TYPE = 415;
-final public static int NOT_FOUND = 404;
-final public static int METHOD_NOT_ALLOWED = 405;
-final public static int BAD_REQUEST = 400;
-final public static int NO_CONTENT = 204;
-final public static int OK = 200;
-final public static int CREATED = 201;
+    public final static int UNPROCESSABLE_CONTENT = 422;
+    public final static int UNSUPPORTED_MEDIA_TYPE = 415;
+    public final static int NOT_FOUND = 404;
+    public final static int METHOD_NOT_ALLOWED = 405;
+    public final static int BAD_REQUEST = 400;
+    public final static int NO_CONTENT = 204;
+    public final static int OK = 200;
+    public final static int CREATED = 201;
 }
